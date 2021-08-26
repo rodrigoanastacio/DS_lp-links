87dda01d28c2569cf154be5100a04ff2bbc62736
Anastacio <rodrigo.anastacio@wundermanthompson.com>
2021-08-26 18:58:40 -0300

    Add meta description

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,21 @@
+import Head from 'next/head';
+
 import Hero from '../components/Hero';
 import Card from '../components/Card';
 
 export default function Home() {
   return (
-    <Hero>
-      <Card />
-    </Hero>
+    <>
+      <Head>
+        <title>Links | Dayane Silva</title>
+        <meta
+          name="description"
+          content="Aqui você encontra links sobre meu trabalho, conteúdos e muito mais."
+        />
+      </Head>
+      <Hero>
+        <Card />
+      </Hero>
+    </>
   );
 }
